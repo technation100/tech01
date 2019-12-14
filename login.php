43981c9321ea709e6957868da6c4c8e44dadcb46
technation <alexted8@gmail.com>
2019-12-14 16:14:26 +0300

    first upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>
+Wellcome
+</title>
+</head>
+<body>
+<form action="index.php" method="post">
+  First name:<br>
+  <input type="text" name="f_name"><br>
+  Last name:<br>
+  <input type="text" name="s_name"><br>
+
+
+        <h2>sex</h2><br>
+  <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+        Registration number<br>
+  <input type="text" name="ele_id"><br>
+ <button type="submit"> begin </button>
+
+</form><br>
+</body>
+</html>
